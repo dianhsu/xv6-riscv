0be39e1240042e3216fab17f167256d2be1c58c8
dianhsu <dianhsu@outlook.com>
2022-10-16 14:37:37 +0800

    Print a page table

--- kernel/defs.h
+++ kernel/defs.h
@@ -171,6 +171,7 @@ int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 uint64*         walk(pagetable_t, uint64, int);
+void            vmprint(pagetable_t);
 // plic.c
 void            plicinit(void);
 void            plicinithart(void);
--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,22 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+void recursive_show(pagetable_t pagetable, int dep){
+  for(int i = 0; i < 512; ++i){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      for(int i = 0; i < dep; ++i) printf(".. ");
+      printf("..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      recursive_show((pagetable_t)child, dep + 1);
+    }else if(pte & PTE_V){
+      for(int i = 0; i < dep; ++i) printf(".. ");
+      printf("..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+  }
+}
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  recursive_show(pagetable, 0);
+}
